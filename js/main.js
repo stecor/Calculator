d29d3ef247f38d8322e8fbdeb16ca415da2e4ae7
Stefano Corra <stefano.corra@hotmail.com>
2017-12-13 16:34:45 -0500

    developed calc

--- js/main.js
+++ js/main.js
@@ -1,31 +1,52 @@
-document.getElementById('num0').addEventListener('click', function(){ myNum(0);});
-document.getElementById('num1').addEventListener('click', function(){ myNum(1);});
-document.getElementById('num2').addEventListener('click', function(){ myNum(2);});
-document.getElementById('num3').addEventListener('click', function(){ myNum(3);});
-document.getElementById('num4').addEventListener('click', function(){ myNum(4);});
-document.getElementById('num5').addEventListener('click', function(){ myNum(5);});
-document.getElementById('num6').addEventListener('click', function(){ myNum(6);});
-document.getElementById('num7').addEventListener('click', function(){ myNum(7);});
-document.getElementById('num8').addEventListener('click', function(){ myNum(8);});
-document.getElementById('num9').addEventListener('click', function(){ myNum(9);});
 var value = document.getElementById('value');
-document.getElementById('comAC').addEventListener("click",myReset);
-var result = 0;
-var numero = 0;
 
+//var result = 0;
+//var calc = "null";
+value.innerHTML = 0;
+var temp = 0;
+var sign = false;
 
+document.getElementById('0').addEventListener('click', function(){ calculator(0);});
+document.getElementById('1').addEventListener('click', function(){ calculator(1);});
+document.getElementById('2').addEventListener('click', function(){ calculator(2);});
+document.getElementById('3').addEventListener('click', function(){ calculator(3);});
+document.getElementById('4').addEventListener('click', function(){ calculator(4);});
+document.getElementById('5').addEventListener('click', function(){ calculator(5);});
+document.getElementById('6').addEventListener('click', function(){ calculator(6);});
+document.getElementById('7').addEventListener('click', function(){ calculator(7);});
+document.getElementById('8').addEventListener('click', function(){ calculator(8);});
+document.getElementById('9').addEventListener('click', function(){ calculator(9);});
+document.getElementById('AC').addEventListener("click", reset);
+document.getElementById('.').addEventListener("click", function(){ calculator(".");});
+document.getElementById('%').addEventListener("click", function(){ calculator("%");});
+document.getElementById('/').addEventListener("click", function(){ calculator("/");});
+document.getElementById('*').addEventListener("click", function(){ calculator("*");});
+document.getElementById('+').addEventListener("click", function(){ calculator("+");});
+document.getElementById('-').addEventListener("click", function(){ calculator("-");});
+document.getElementById('=').addEventListener("click", function(){ calculator("=");});
 
-
-function myReset(){
+function reset(){
   value.innerHTML = 0;
+  temp = 0;
 }
 
-function myNum(num){
-  if ( value.innerHTML == "0"){
-    value.innerHTML = num ;
-  }else{
-    value.innerHTML += num ;
-  }
-};
+function calculator(data){
 
-value.innerHTML = result;
+  if(value.innerHTML === "0"){
+    value.innerHTML="";
+  }
+
+  switch (data) {
+      case ("="):   var calcule = eval(temp);
+                    value.innerHTML = calcule;
+                    console.log(temp);
+                    break;
+
+      default:
+                        value.innerHTML = "";
+                        value.innerHTML += data;
+                        temp += data;
+
+  }
+
+}

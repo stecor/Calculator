d4a28b42696924f557b6049b4f02dd2dcf022881
Stefano Corra <stefano.corra@hotmail.com>
2018-01-13 04:27:13 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 var value = document.getElementById('value');
-
-//var result = 0;
-//var calc = "null";
-value.innerHTML = 0;
-var temp = 0;
+var temp = [];
 var sign = false;
 
+//Linking button values 
 document.getElementById('0').addEventListener('click', function(){ calculator(0);});
 document.getElementById('1').addEventListener('click', function(){ calculator(1);});
 document.getElementById('2').addEventListener('click', function(){ calculator(2);});
@@ -14,9 +11,10 @@ document.getElementById('4').addEventListener('click', function(){ calculator(4)
 document.getElementById('5').addEventListener('click', function(){ calculator(5);});
 document.getElementById('6').addEventListener('click', function(){ calculator(6);});
 document.getElementById('7').addEventListener('click', function(){ calculator(7);});
-document.getElementById('8').addEventListener('click', function(){ calculator(8);});
+document.getElementById('8').addEventListener('click', function(){ calculator("8");});
 document.getElementById('9').addEventListener('click', function(){ calculator(9);});
-document.getElementById('AC').addEventListener("click", reset);
+document.getElementById('AC').addEventListener("click", allClear);
+document.getElementById('CE').addEventListener("click", clearEntry);
 document.getElementById('.').addEventListener("click", function(){ calculator(".");});
 document.getElementById('%').addEventListener("click", function(){ calculator("%");});
 document.getElementById('/').addEventListener("click", function(){ calculator("/");});
@@ -25,28 +23,40 @@ document.getElementById('+').addEventListener("click", function(){ calculator("+
 document.getElementById('-').addEventListener("click", function(){ calculator("-");});
 document.getElementById('=').addEventListener("click", function(){ calculator("=");});
 
-function reset(){
-  value.innerHTML = 0;
-  temp = 0;
+// Erase all operation
+function allClear(){
+  value.innerHTML = "";
+  temp = [];
+}
+
+// Erase just the last operation
+function clearEntry(){
+  temp.pop();
+  value.innerHTML = temp.join("");
+  console.log(temp);
 }
 
 function calculator(data){
 
-  if(value.innerHTML === "0"){
-    value.innerHTML="";
-  }
-
   switch (data) {
-      case ("="):   var calcule = eval(temp);
-                    value.innerHTML = calcule;
-                    console.log(temp);
-                    break;
+      
+      // Case equal finalize operation
+      case ("="):   
+      
+            var str = temp.join("");
+            var calcule = eval(str);
+            value.innerHTML = calcule;
+            console.log(calcule);
+            break;
 
-      default:
-                        value.innerHTML = "";
-                        value.innerHTML += data;
-                        temp += data;
-
+      // Enter values into array 
+      default:       
+                        
+            value.innerHTML += data;
+            if(data == "%"){
+              temp.push("/100");      
+            }else{
+              temp.push(data);          
+            }              
   }
-
 }
